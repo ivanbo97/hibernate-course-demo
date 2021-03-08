2e3d534115ad35db4562c496996ad4b30c9541a6
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-08 15:40:53 +0200

    Adding basic java object persistence into DB.

---
+++ hibernate-demo-course/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,47 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Student.class)
+								 .buildSessionFactory();
+		
+		//create a session
+		Session session = factory.getCurrentSession();
+		try {
+			
+			
+			// create a student object
+			System.out.println("Creating a new student object...");
+			Student student = new Student("Paul","Wall","paul@luv2code.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the student object
+			System.out.println("Saving the student...");
+			session.save(student);
+			
+			// commit the transaction
+			System.out.println("The transaction is done.");
+			session.getTransaction().commit();
+				
+			
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
+
