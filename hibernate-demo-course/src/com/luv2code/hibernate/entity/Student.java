7315addcd33ccc512305b0dce866cf55b9af7dae
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-09 05:39:06 +0200

    Additional annotation for primary key field.

--- hibernate-demo-course/src/com/luv2code/hibernate/entity/Student.java
+++ hibernate-demo-course/src/com/luv2code/hibernate/entity/Student.java
@@ -2,6 +2,8 @@ package com.luv2code.hibernate.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class Student {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
@@ -66,6 +69,6 @@ public class Student {
 
 	@Override
 	public String toString() {
-		return "Student [ids=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
 }
